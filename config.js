4a0a2aa4e9a7b34573903bcb36ba748e78bdc8fb
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-12-05 11:19:45 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290"], // your id
+  ownerUsernames: ["HIMU"],//Your Username
+  ownerNumber: "+8801332754898",//Your Number
+  admin: ["6800909814"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
+  telegramBotToken: "7311087112:AAG6OYCrL0V7ueQkKSAXeRrDmg-GqyUQVps",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
